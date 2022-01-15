28ba408431a9b92174b102d81fb71733f2b95faa
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-16 02:17:52 +0330

    js added

---
+++ counter/App.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+const value = document.querySelector ( "#value" );
+const btns = document.querySelectorAll ( ".btn" );
+
+const btns_1 = [ ... btns ]
+btns_1.forEach ( ( item ) => {
+    item.addEventListener ( "click" , function ( e ) {
+        const styles = e.currentTarget.classList;
+        if ( styles.contains ( "decrease" ) ) {
+            count --;
+        } else if ( styles.contains ( "increase" ) ) {
+            count ++;
+        } else {
+            count = 0;
+        }
+        if ( count > 0 ){
+            value.style.color = `green`;
+        }
+        if ( count < 0 ){
+            value.style.color = `red`;
+        }
+        value.textContent = count;
+    } )
+} )
