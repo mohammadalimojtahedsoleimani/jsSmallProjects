e36342f20c7b83f16dd11288f57ab5dfa21e0423
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-20 02:27:26 +0330

    some changes

--- Questions/main.js
+++ Questions/main.js
@@ -1,7 +1,17 @@
-const questions = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question-btn");
+const allOfThem = document.querySelectorAll(".question");
 
 questions.forEach((item)=>{
     item.addEventListener("click",(event)=>{
+        allOfThem.forEach((check)=>{
+
+
+            if ( check !== item ){
+                check.classList.remove("show-text");
+                console.log (check)
+                console.log (item)
+            }
+        })
        const question = event.currentTarget.parentElement.parentElement;
        question.classList.toggle("show-text");
     })
